c853611849eb66f6448413142c5b4ba6cc0f7427
Aloïs Gaucher <contact@alois-gaucher.fr>
2024-05-23 16:49:10 +0200

    copy the images to a backup folder before processing

--- detect_faces.py
+++ detect_faces.py
@@ -32,14 +32,23 @@ def count_people_in_image(image_path):
 
 def rename_and_copy_images_in_folder(folder_path):
     output_folder_path = os.path.join(folder_path, "../output")
+    backup_folder_path = os.path.join(folder_path, "../images_backup")
     if not os.path.exists(output_folder_path):
         os.makedirs(output_folder_path)
+    if not os.path.exists(backup_folder_path):
+        os.makedirs(backup_folder_path)
 
     image_found = False
     for filename in os.listdir(folder_path):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG')):
             image_found = True
             image_path = os.path.join(folder_path, filename)
+
+            # Copy the original file to the backup folder
+            backup_image_path = os.path.join(backup_folder_path, filename)
+            shutil.copy(image_path, backup_image_path)
+            print(f"Copied '{filename}' to backup folder")
+
             person_count = count_people_in_image(image_path)
             file_base, file_extension = os.path.splitext(filename)
             new_filename = f"{file_base}_({person_count} personnes){file_extension}"
@@ -55,6 +64,10 @@ def rename_and_copy_images_in_folder(folder_path):
                 copy_image_path = os.path.join(output_folder_path, copy_filename)
                 shutil.copy(new_image_path, copy_image_path)
                 print(f"Created copy '{copy_filename}'")
+
+            # Remove the original file from the original folder
+            os.remove(image_path)
+            print(f"Removed '{filename}' from original folder")
 
     if not image_found:
         print("No pictures found")
